f1e52ba82cc2c5f4a8418f2887dae3592bc001ef
Bátonyi Péter <batonyi.peter@danubiusinfo.hu>
2018-12-20 15:26:27 +0100

    Coverage fix

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/exception/TooManyPlayerException.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/exception/TooManyPlayerException.java
@@ -2,22 +2,7 @@ package com.adaptionsoft.games.uglytrivia.exception;
 
 public class TooManyPlayerException extends RuntimeException {
 
-    public TooManyPlayerException() {
-    }
-
     public TooManyPlayerException(String message) {
         super(message);
-    }
-
-    public TooManyPlayerException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public TooManyPlayerException(Throwable cause) {
-        super(cause);
-    }
-
-    public TooManyPlayerException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
