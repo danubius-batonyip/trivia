6989fc7f1884fe64cc339b2ca510b54dd484ac91
Bátonyi Péter <batonyi.peter@danubiusinfo.hu>
2018-12-20 11:52:03 +0100

    Output abstraction

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -4,9 +4,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import static org.junit.Assert.*;
 
 public class GameTest {
